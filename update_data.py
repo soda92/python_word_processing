3f10abe67ff5d18057e928e0c2a72cb4941a0437
SodaCris <18463922396@163.com>
2021-09-28 21:44:00 +0800

    refactor

--- update_data.py
+++ update_data.py
@@ -1,142 +1,64 @@
-# 要添加一个新单元，输入 '# %%'
-# 要添加一个新的标记单元，输入 '# %% [markdown]'
-# %%
 from openpyxl import load_workbook
 from docx import Document
-import pandas as pd
+from pathlib import Path
+import os
 
 
-# %%
-df = pd.read_excel("订单数据.xlsx", header=None)
+def get_output_path(filename):
+    curr_dir = Path(__file__).resolve().parent
+    out_dir = Path.joinpath(curr_dir, 'out')
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+    out_file_path = Path.joinpath(out_dir, filename)
+    return out_file_path
 
 
-# %%
-mapping = dict(zip(df[0], df[1]))
+def read_data(filename):
+    mapping = {}
+    wb = load_workbook(filename=filename)
+    ws = wb.active
+
+    for row in list(range(1, 20)):
+        for col in list(range(1, 20)):
+            val = ws.cell(row=row, column=col).value
+            if val != None:
+                cell_right = ws.cell(row=row, column=col+1)
+                crval = cell_right.value
+                if crval != None:
+                    mapping[val] = crval
+    wb.close()
+    return mapping
 
 
-# %%
-mapping
+def modify_xlsx(mapping, filename):
+    wb = load_workbook(filename=filename)
+    ws = wb.active
+
+    for row in list(range(1, 20)):
+        for col in list(range(1, 20)):
+            val = ws.cell(row=row, column=col).value
+            cell_right = ws.cell(row=row, column=col+1)
+            crval = cell_right.value
+            if crval == None:
+                if val in mapping:
+                    print(f"写入数据：{val}->{mapping[val]}")
+                    ws.cell(row=row, column=col+1, value=mapping[val])
+    wb.save(get_output_path(filename))
 
 
-# %%
-doc = Document(docx="订单信息.docx")
+def modify_docx(mapping, filename):
+    doc = Document(docx=filename)
+    table = doc.tables[0]
+
+    for i in range(len(table.rows)):
+        for j in range(len(table.rows[i].cells)):
+            text = table.cell(i, j).text
+            if text in mapping:
+                print(f"写入数据：{text}->{mapping[text]}")
+                table.cell(i, j+1).text = str(mapping[text])
+    doc.save(get_output_path(filename))
 
 
-# %%
-table = doc.tables[0]
-
-
-# %%
-type(table.rows)
-
-
-# %%
-list_data = []
-
-
-# %%
-len_ = len(table.rows)
-for i in range(len_):
-    row = table.rows[i]
-    data = []
-    for cell in row.cells:
-        data.append(cell.text)
-    list_data.append(data)
-
-
-# %%
-list_data
-
-
-# %%
-wb = load_workbook(filename="订单信息.xlsx")
-
-
-# %%
-ws = wb.active
-
-
-# %%
-excel_data = []
-for row in list(range(1, 20)):
-    row_data = []
-    for col in list(range(1, 20)):
-        row_data.append(ws.cell(row=row, column=col).value)
-    excel_data.append(row_data)
-
-
-# %%
-df_excel = pd.DataFrame(excel_data).dropna(axis="index", how="all")
-
-
-# %%
-df_excel = df_excel.dropna(axis="columns", how="all")
-
-
-# %%
-df_excel
-
-
-# %%
-ws.cell(row=2, column=5).value == None
-
-
-# %%
-for row in list(range(1, 20)):
-    row_data = []
-    for col in list(range(1, 20)):
-        val = ws.cell(row=row, column=col).value
-        cell_right = ws.cell(row=row, column=col+1)
-        crval = cell_right.value
-        if crval == None:
-            if val in mapping:
-                ws.cell(row=row, column=col+1, value = mapping[val])
-        else:
-            mapping[val]=crval
-
-
-# %%
-mapping
-
-
-# %%
-wb.save("out/订单信息.xlsx")
-
-
-# %%
-df2=pd.DataFrame(list_data)
-
-
-# %%
-df2
-
-
-# %%
-len(df2.columns)
-
-
-# %%
-len(df2.index)
-
-
-# %%
-mapping
-
-
-# %%
-for i in range(len(table.rows)):
-    for j in range(len(table.rows[i].cells)):
-        text=table.cell(i, j).text
-        if text in mapping:
-            print(text)
-            try:
-                table.cell(i, j+1).text=str(mapping[text])
-            except:
-                pass
-
-
-# %%
-doc.save("out/订单信息.docx")
-
-
-# %%
+data = read_data("订单数据.xlsx")
+modify_xlsx(data, "订单信息.xlsx")
+modify_docx(data, "订单信息.docx")
